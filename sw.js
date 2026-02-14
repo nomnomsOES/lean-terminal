2e63cfea7abab573527ab75a711d3c10475d21f1
nomnomsOES <raul@steerandsave.com>
2026-02-13 16:23:46 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open("lean-v1").then((cache) => cache.addAll(["./", "./index.html", "./manifest.json"])));
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+});
